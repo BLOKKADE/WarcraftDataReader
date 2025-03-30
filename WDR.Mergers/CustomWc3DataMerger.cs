986b3344b1d903cb558ca1ef5b06bbb81152086c
BLOKKADE <mathijz095@gmail.com>
2025-03-30 22:20:10 +0200

    Add FieldName to each level and set to correct level

--- WDR.Mergers/CustomWc3DataMerger.cs
+++ WDR.Mergers/CustomWc3DataMerger.cs
@@ -2,12 +2,13 @@
 
 namespace WDR.Mergers;
 
-public static class CustomWc3DataMerger
+public static partial class CustomWc3DataMerger
 {
     public static Wc3Data ExpandCustomData(Wc3Data defaultData, Wc3Data customData)
     {
         foreach (var origObj in defaultData.Original)
         {
+
             var customOriginalObj = customData.Original.SingleOrDefault(x => x.Code == origObj.Code);
             var customCustomObjs = customData.Custom.Where(x => x.OriginalCode == origObj.Code);
 
@@ -20,6 +21,12 @@ public static class CustomWc3DataMerger
             {
                 // if found then add all fields not edited in the custom data
                 Merge(origObj, customOriginalObj);
+            }
+
+            if (customCustomObjs.Any(x => x.Code == "A0DX"))
+            {
+                var test = 1;
+                var stri = test.ToString();
             }
 
             // add all unedited fields from the original obj to all custom objects based on it
@@ -38,15 +45,30 @@ public static class CustomWc3DataMerger
     {
         foreach (var defaultField in defaultData.Fields)
         {
-            var customField = customData.Fields.FirstOrDefault(x => x.Id == defaultField.Id);
-            if (customField == null)
+            var customField = customData.Fields.Where(x => x.Id == defaultField.Id);
+            if (customField?.Any() != true)
             {
                 customData.Fields.Add(defaultField);
             }
             else
             {
-                customField.FieldName ??= defaultField.FieldName;
+                foreach (var field in customField)
+                {
+                    if (defaultField.FieldName != null && field.Level != 0)
+                    {
+                        var match = FieldNameRegex().Match(defaultField.FieldName);
+                        if (match.Success)
+                        {
+                            var namePart = match.Groups[1].Value;
+                            field.FieldName = $"{namePart}{field.Level}";
+                        }
+                    }
+                    field.FieldName ??= defaultField.FieldName;
+                }
             }
         }
     }
+
+    [System.Text.RegularExpressions.GeneratedRegex(@"^(.*?)(\d+)$")]
+    private static partial System.Text.RegularExpressions.Regex FieldNameRegex();
 }
